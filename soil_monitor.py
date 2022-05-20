76307111184a1be57f36f569277aeb4841816ede
Chris Perkins <cjperks@gmail.com>
2022-05-20 22:42:27 +0100

    test commit

---
+++ soil_monitor.py
@@ -0,0 +1,15 @@
+# monitoring daemon for reading the soil humidity
+# and applying water as required
+
+# general initialisation
+
+# desired humidity
+ds = 1
+
+
+
+# get the number of active sensors
+
+# check against desired value
+
+# apply water if lower than desired value
